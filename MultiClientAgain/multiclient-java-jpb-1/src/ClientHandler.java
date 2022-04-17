5b279638a1402bcbae3e56d21eafabf874eb0437
JosephF24 <87243590+JosephF24@users.noreply.github.com>
2022-04-16 21:15:01 -0400

    ClientThreadChanges

--- MultiClientAgain/multiclient-java-jpb-1/src/ClientHandler.java
+++ MultiClientAgain/multiclient-java-jpb-1/src/ClientHandler.java
@@ -1,33 +1,91 @@
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class loginsList {
+    public String Username;
+    public String PW;
+}
 
 
 
 //Runnable class allows us to create a task
 //to be run on a thread
-public class ClientHandler implements Runnable {
+public class ClientHandler extends Thread implements Runnable {
     private Socket socket;  //connected socket
     private ServerSocket serverSocket;  //server's socket
     private int clientNumber;
-    
+    static Vector<ClientHandler> handlers = new Vector<ClientHandler>(20);
+    private BufferedReader takeText;
+    private PrintWriter outputText;
+    private String IDName;
+    private String IPAddress;
+
+    private boolean isRoot = false;
+    private boolean isLogin = false;
+    private String str;
+
+
+    loginsList Ll[] = new loginsList[5];
+    File messagefile = new File("removethisifnotused");
+
+    private BufferedReader bR = new BufferedReader(new FileReader(messagefile));
+    int numofMessages = 0;
+    Vector messages = new Vector();
+    int messageTracker = 0;
+
+    public ClientHandler(Socket socket) throws IOException {
+    this.socket = socket;
+    takeText = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    outputText = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
     //create an instance
-    public ClientHandler(int clientNumber, Socket socket, ServerSocket serverSocket) {
+    public ClientHandler(int clientNumber, Socket socket, ServerSocket serverSocket) throws FileNotFoundException {
         this.socket = socket;
         this.serverSocket = serverSocket;
         this.clientNumber = clientNumber;
     }//end ctor
-    
-    
+
+
     //run() method is required by all
     //Runnable implementers
     @Override
     public void run() {
         //run the thread in here
         try {
+            while ((str = bR.readLine()) != null) {
+                messages.add(str);
+                numofMessages++;
+            }
+        }catch (IOException ex) {
+            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+            for(int i = 0; i < 5; i++)
+            {
+                Ll[i] = new loginsList();
+            }
+
+            //input login text and passwords here
+
+            boolean firstLine = false;
+            boolean firstRun = false;
+
+            String line;
+
+            synchronized(handlers)
+            {
+                handlers.addElement(this);
+            }
+
+            try
+            {
+
             DataInputStream inputFromClient =
                     new DataInputStream(socket.getInputStream());
             
@@ -46,6 +104,17 @@ public class ClientHandler implements Runnable {
                             clientNumber);
                     outputToClient.writeUTF("hello client " +
                             clientNumber + "!");
+                }
+                else if(strReceived.equalsIgnoreCase("message"))
+                {
+                    if(!isLogin)
+                    {
+                        System.out.println("Not logged in, please log in!");
+                    }
+                    else
+                    {
+
+                    }
                 }
                 else if(strReceived.equalsIgnoreCase("quit")) {
                     System.out.println("Shutting down server...");
@@ -66,7 +135,7 @@ public class ClientHandler implements Runnable {
         catch(IOException ex) {
             ex.printStackTrace();
         }//end try-catch
-        
+
     }//end run
     
 }//end ClientHandler
