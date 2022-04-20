789c3ae43f89b30259c61116323d2a6df9192a8c
Amr Mashrah <mashrah25@gmail.com>
2022-04-19 22:26:55 -0400

    Comments and Formating

--- MultiClientAgain/multiclient-java-jpb-1/src/ClientJPB1.java
+++ MultiClientAgain/multiclient-java-jpb-1/src/ClientJPB1.java
@@ -1,40 +1,24 @@
-
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
-
-
-
 public class ClientJPB1 {
-    
     private static final int SERVER_PORT = 8765;
-
     public static void main(String[] args) throws UnknownHostException, IOException
     {
         InetAddress ip = InetAddress.getByName("localhost");
-
         DataOutputStream toServer;
         DataInputStream fromServer;
-        Scanner input = 
-                new Scanner(System.in);
+        Scanner input =  new Scanner(System.in);
         Socket s = new Socket(ip, SERVER_PORT);
         String message;
-        
         //attempt to connect to the server
         try {
-            Socket socket = 
-                    new Socket("localhost", SERVER_PORT);
-
+            Socket socket =  new Socket("localhost", SERVER_PORT);
             //create input stream to receive data
             //from the server
-            fromServer = 
-                    new DataInputStream(socket.getInputStream());
-            
-            toServer =
-                    new DataOutputStream(socket.getOutputStream());
-            
-             
+            fromServer = new DataInputStream(socket.getInputStream());
+            toServer = new DataOutputStream(socket.getOutputStream());
              while(true) {
                 System.out.print("Send command to server:\t");
                 message = input.nextLine();
@@ -42,7 +26,6 @@ public class ClientJPB1 {
                 if(message.equalsIgnoreCase("quit")) {
                     break;
                 }
-                
                 //received message:
                 message = fromServer.readUTF();
                  System.out.println("Server says: " + message);
@@ -52,39 +35,9 @@ public class ClientJPB1 {
                      break;
                  }
              }
-             
         }
         catch(IOException ex) {
             ex.printStackTrace();
         }//end try-catch
-        
-        
     }//end main
 }
-//below is our attempts to get login to work with our project.
-//            while(!loginTest) {
-//                System.out.println("Please type in your username followed by your password.");
-//                message = input.nextLine();
-//                toServer.writeUTF(message);
-//                message = fromServer.readUTF();
-//                if (message.equalsIgnoreCase("working")) {
-//                    loginTest = true;
-//                } else {
-//                    System.out.println("Incorrect username and/or password. Please try again.");
-//                }
-//                System.out.println(loginTest);
-//                System.out.print(message);
-//            }
-//            while (loginTest) {
-//                System.out.print("Send command to server:\t");
-//                message = input.nextLine();
-//                toServer.writeUTF(message);
-//                if (message.equalsIgnoreCase("200 OK")) {
-//                    System.out.print(message);
-//                    break;
-//                }
-//
-//                //received message:
-//                message = fromServer.readUTF();
-//                System.out.print(message);
-//            }
